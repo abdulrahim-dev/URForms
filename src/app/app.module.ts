1763019ff651325550e9e6a39916a108c37669e5
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2018-12-14 11:28:43 +0530

    ng g component app/layouts/admin-layout
    ng g module app/layouts/admin-layout/admin-layout
    
    create a file for routing in admin-layout folder
    
    npm i @angular/material
    import required items for material to AdminLayoutModule
    
    create app.routing.ts file
    -- added routes as children here
    
    app.module.ts --> add routingmodule to main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app.routing';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,AdminLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
